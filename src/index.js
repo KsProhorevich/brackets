6278e0333887eba7eb7ef0f1309f5ac27904fde5
Your Name <you@example.com>
2026-05-10 22:40:30 +0300

    feat: implement brackets sequence validator

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
-};
+function check(str, bracketsConfig) {
+  const stack = [];
+  const bracketsMap = new Map();
+  const openBrackets = new Set();
+  const closeBrackets = new Set();
+
+  bracketsConfig.forEach(([open, close]) => {
+    bracketsMap.set(close, open);
+    openBrackets.add(open);
+    closeBrackets.add(close);
+  });
+
+  for (let i = 0; i < str.length; i += 1) {
+    const char = str[i];
+
+    if (openBrackets.has(char)) {
+      if (char === bracketsMap.get(char)) {
+        if (stack.length > 0 && stack[stack.length - 1] === char) {
+          stack.pop();
+        } else {
+          stack.push(char);
+        }
+      } else {
+        stack.push(char);
+      }
+    } else if (closeBrackets.has(char)) {
+      const expectedOpen = bracketsMap.get(char);
+      if (stack.length === 0 || stack.pop() !== expectedOpen) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+}
+
+module.exports = check;
